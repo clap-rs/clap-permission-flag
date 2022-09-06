fab6d5784d63680e944fc18ac489f56024d50e2a
Matthias Beyer <mail@beyermatthias.de>
2022-09-06 13:13:14 +0200

    cargo fmt
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- examples/main.rs
+++ examples/main.rs
@@ -6,12 +6,12 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
-  #[structopt(flatten)]
-  permission: clap_permission_flag::Permission,
+    #[structopt(flatten)]
+    permission: clap_permission_flag::Permission,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let args = Cli::from_args();
-  args.permission.drop()?;
-  Ok(())
+    let args = Cli::from_args();
+    args.permission.drop()?;
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,95 +16,95 @@ use std::path::PathBuf;
 
 #[cfg(unix)]
 mod supported {
-  use super::*;
+    use super::*;
 
-  /// Drop permissions of a CLI using clap.
-  #[derive(Parser, Debug)]
-  pub struct Permission {
-    /// Change the process user
-    #[clap(short = 'u', long = "user", parse(from_os_str))]
-    user: Option<OsString>,
-    /// Change the process group
-    #[clap(short = 'g', long = "group", parse(from_os_str))]
-    group: Option<OsString>,
-    /// Change the process root directory
-    #[cfg(feature = "chroot")]
-    #[clap(long = "chroot", parse(from_os_str))]
-    chroot: Option<PathBuf>,
-  }
+    /// Drop permissions of a CLI using clap.
+    #[derive(Parser, Debug)]
+    pub struct Permission {
+        /// Change the process user
+        #[clap(short = 'u', long = "user", parse(from_os_str))]
+        user: Option<OsString>,
+        /// Change the process group
+        #[clap(short = 'g', long = "group", parse(from_os_str))]
+        group: Option<OsString>,
+        /// Change the process root directory
+        #[cfg(feature = "chroot")]
+        #[clap(long = "chroot", parse(from_os_str))]
+        chroot: Option<PathBuf>,
+    }
 
-  impl Permission {
-    /// Drop privileges.
-    pub fn drop(self) -> Result<(), Error> {
-      let mut drop = privdrop::PrivDrop::default();
+    impl Permission {
+        /// Drop privileges.
+        pub fn drop(self) -> Result<(), Error> {
+            let mut drop = privdrop::PrivDrop::default();
 
-      if let Some(user) = self.user {
-        drop = drop.user(&user);
-      }
+            if let Some(user) = self.user {
+                drop = drop.user(&user);
+            }
 
-      if let Some(group) = self.group {
-        drop = drop.group(&group);
-      }
+            if let Some(group) = self.group {
+                drop = drop.group(&group);
+            }
 
-      #[cfg(feature = "chroot")]
-      {
-        if let Some(chroot) = self.chroot {
-          drop = drop.chroot(&chroot);
+            #[cfg(feature = "chroot")]
+            {
+                if let Some(chroot) = self.chroot {
+                    drop = drop.chroot(&chroot);
+                }
+            }
+
+            drop.apply()?;
+            Ok(())
         }
-      }
 
-      drop.apply()?;
-      Ok(())
-    }
+        /// Get the user.
+        pub fn user(&self) -> &Option<OsString> {
+            &self.user
+        }
 
-    /// Get the user.
-    pub fn user(&self) -> &Option<OsString> {
-      &self.user
-    }
+        /// Get the group.
+        pub fn group(&self) -> &Option<OsString> {
+            &self.group
+        }
 
-    /// Get the group.
-    pub fn group(&self) -> &Option<OsString> {
-      &self.group
+        /// Get the chroot directory.
+        #[cfg(feature = "chroot")]
+        pub fn chroot(&self) -> &Option<PathBuf> {
+            &self.chroot
+        }
     }
-
-    /// Get the chroot directory.
-    #[cfg(feature = "chroot")]
-    pub fn chroot(&self) -> &Option<PathBuf> {
-      &self.chroot
-    }
-  }
 }
 
 #[cfg(not(unix))]
 mod stub {
-  use super::*;
+    use super::*;
 
-  /// Drop permissions of a CLI using clap.
-  #[derive(Parser, Debug)]
-  pub struct Permission {}
+    /// Drop permissions of a CLI using clap.
+    #[derive(Parser, Debug)]
+    pub struct Permission {}
 
-  impl Permission {
-    /// Drop privileges.
-    pub fn drop(self) -> Result<(), Error> {
-      Ok(())
+    impl Permission {
+        /// Drop privileges.
+        pub fn drop(self) -> Result<(), Error> {
+            Ok(())
+        }
+
+        /// Get the user.
+        pub fn user(&self) -> &Option<OsString> {
+            &None
+        }
+
+        /// Get the group.
+        pub fn group(&self) -> &Option<OsString> {
+            &None
+        }
+
+        /// Get the chroot directory.
+        #[cfg(feature = "chroot")]
+        pub fn chroot(&self) -> &Option<PathBuf> {
+            &None
+        }
     }
-
-    /// Get the user.
-    pub fn user(&self) -> &Option<OsString> {
-      &None
-    }
-
-    /// Get the group.
-    pub fn group(&self) -> &Option<OsString> {
-      &None
-    }
-
-    /// Get the chroot directory.
-    #[cfg(feature = "chroot")]
-    pub fn chroot(&self) -> &Option<PathBuf> {
-      &None
-    }
-  }
 }
 
 #[cfg(unix)]
